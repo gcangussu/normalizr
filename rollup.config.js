68c42e14afbecb7a9564f367ae07483d0bab53e7
Gabriel Cangussu <gabrielcangussu@gmail.com>
2020-11-19 11:21:10 -0300

    v3.6.3

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import filesize from 'rollup-plugin-filesize';
-import { name } from './package.json';
 import { terser } from 'rollup-plugin-terser';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -13,9 +12,9 @@ export default {
   output: [
     { file: `${destBase}${destExtension}`, format: 'cjs' },
     { file: `${destBase}.es${destExtension}`, format: 'es' },
-    { file: `${destBase}.umd${destExtension}`, format: 'umd', name },
-    { file: `${destBase}.amd${destExtension}`, format: 'amd', name },
-    { file: `${destBase}.browser${destExtension}`, format: 'iife', name },
+    { file: `${destBase}.umd${destExtension}`, format: 'umd', name: 'normalizr' },
+    { file: `${destBase}.amd${destExtension}`, format: 'amd', name: 'normalizr' },
+    { file: `${destBase}.browser${destExtension}`, format: 'iife', name: 'normalizr' },
   ],
   plugins: [babel({}), isProduction && terser(), filesize()].filter(Boolean),
 };
